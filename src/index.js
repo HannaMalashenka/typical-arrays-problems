4fcfb6e6abeb3610096b24626b1c4c5463b58b6e
anika <hannamalashenka@gmail.com>
2020-10-03 22:20:47 +0300

    feat: add all functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
 
 exports.min = function min (array) {
-  return 0;
+  	if (!Array.isArray(array) || array.length === 0) { 
+		return 0; 
+  	}
+  	else { 
+  		let min = array[0];
+    	for (let i = 0; i < array.length; i++) {
+    		min = min > array[i] ? array[i] : min
+		}
+		return min;
+  	}
 }
 
 exports.max = function max (array) {
-  return 0;
+  	if (!Array.isArray(array) || array.length === 0) { 
+  		return 0; 
+  	}
+	else { 
+		let max = array[0];
+	    for (let i = 0; i < array.length; i++) {
+	    	max = max < array[i] ? array[i] : max
+		}
+		return max;
+	}
 }
 
 exports.avg = function avg (array) {
-  return 0;
+	if (!Array.isArray(array) || array.length === 0) { 
+  		return 0; 
+  	}
+  	else { 
+  		let arraySum = array.reduce((a, b) => a + b);
+  		return arraySum / array.length;
+  	}
 }
